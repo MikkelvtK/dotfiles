804faff3db60dcc47ec1572ef3718a4c1f1f08bf
MikkelvtK <michielvtklooster@gmail.com>
2023-05-26 13:29:18 +0200

    Add random quality of life plugins

--- .config/nvim-dev/lua/plugins/completion/init.lua
+++ .config/nvim-dev/lua/plugins/completion/init.lua
@@ -9,6 +9,7 @@ return {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
+      "onsails/lspkind.nvim",
     },
     config = function()
       local cmp = require("cmp")
@@ -24,11 +25,29 @@ return {
           documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-j>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end, {
+            "i", "s", "c",
+          }),
+          ["<C-k>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()
+            end
+          end, {
+            "i", "s", "c",
+          }),
         }),
         sources = cmp.config.sources({
           { name = "nvim-lsp" },
@@ -37,7 +56,22 @@ return {
         }, {
           { name = "buffer" }, 
         }),
+        formatting = {
+          format = function(entry, vim_item)
+            if vim.tbl_contains({ "path" }, entry.source.name) then
+              local icon, hl_group = require("nvim-web-devicons").get_icon(entry:get_completion_item().label)
+              if icon then
+                vim_item.kind = icon
+                vim_item.kind_hl_group = hl_group
+                return vim_item
+              end
+            end
+            return require("lspkind").cmp_format({ with_text = false })(entry, vim_item)
+          end,
+        },
       })
     end,
   },
+
+  -- TODO: Add support for LuaSnip
 }
--- .config/nvim-dev/lua/plugins/init.lua
+++ .config/nvim-dev/lua/plugins/init.lua
@@ -11,5 +11,33 @@ return {
     config = function()
       vim.cmd([[colorscheme tokyonight-storm]])
     end,
-  }
+  },
+  {
+    "rcarriga/nvim-notify",
+    event = "VeryLazy",
+    enabled = true,
+    config = { default = true }, -- same as config = true
+  },
+  {
+    "stevearc/dressing.nvim",
+    event = "VeryLazy",
+    config = true,
+  },
+  {
+    "TimUntersberger/neogit",
+    cmd = "Neogit",
+    config = {
+      integrations = { diffview = true },
+    },
+    keys = {
+      { "<leader>gs", "<cmd>Neogit kind=floating<cr>", desc = "Status" },
+    },
+  },
+  {
+    "echasnovski/mini.pairs",
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("mini.pairs").setup(opts)
+    end,
+  },
 }
