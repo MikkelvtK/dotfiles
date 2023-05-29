95f86d1a033c20cdaa1282b0993b778f90727276
MikkelvtK <mvtklooster@hotmail.com>
2023-05-29 11:30:16 +0200

    Fix bug with Comment.nvim, change keymapping completion

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,17 @@
+-- TODO: Add function jumping
+
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	event = "BufReadPre",
-	sync_install = false,
-	auto_install = true,
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "lua", "go" },
-			highlight = {
-				enable = true,
-			},
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = "BufReadPre",
+  sync_install = false,
+  auto_install = true,
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "lua", "go" },
+      highlight = {
+        enable = true,
+      },
+    })
+  end,
 }
