990d686ff0f2324766b9674911b71a2ab52a1350
MikkelvtK <mvtklooster@hotmail.com>
2023-05-29 20:32:55 +0200

    Add Nord colorscheme

--- .config/nvim-dev/lua/plugins/colorscheme.lua
+++ .config/nvim-dev/lua/plugins/colorscheme.lua
@@ -4,8 +4,13 @@ return {
     "sainnhe/everforest",
     lazy = false,
     priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme everforest]])
-    end,
   },
+  {
+    "gbprod/nord.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme nord]])
+    end
+  }
 }
--- .config/nvim-dev/lua/plugins/neogen.lua
+++ .config/nvim-dev/lua/plugins/neogen.lua
@@ -5,6 +5,7 @@ return {
     snippet_engine = "luasnip",
     enabled = true,
     languages = {
+      -- TODO: Add Go support
       lua = {
         template = {
           annotation_convention = "ldoc",
@@ -13,9 +14,9 @@ return {
     },
   },
   keys = {
-    { "<leader>cgd", function() require("neogen").generate() end, desc = "Annotation", },
+    { "<leader>cgd", function() require("neogen").generate() end,                  desc = "Annotation", },
     { "<leader>cgc", function() require("neogen").generate { type = "class" } end, desc = "Class", },
-    { "<leader>cgf", function() require("neogen").generate { type = "func" } end, desc = "Function", },
-    { "<leader>cgt", function() require("neogen").generate { type = "type" } end, desc = "Type", },
+    { "<leader>cgf", function() require("neogen").generate { type = "func" } end,  desc = "Function", },
+    { "<leader>cgt", function() require("neogen").generate { type = "type" } end,  desc = "Type", },
   },
 }
