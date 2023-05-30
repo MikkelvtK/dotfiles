5948f98ce9ff7de08388890d433448a1d99d6612
MikkelvtK <mvtklooster@hotmail.com>
2023-05-30 11:16:00 +0200

    Add config to teleschop

--- .config/nvim-dev/lua/plugins/telescope.lua
+++ .config/nvim-dev/lua/plugins/telescope.lua
@@ -3,12 +3,35 @@ return {
   tag = "0.1.1",
   cmd = "Telescope",
   dependencies = { 'nvim-lua/plenary.nvim' },
-  config = true,
   keys = {
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-    { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
-    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-    { "<leader>f/", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
-    { "<leader>fc", function() require("telescope.builtin").colorscheme({enable_preview = true}) end, desc = "Colorscheme", },
-  }
+    { "<leader>fr", "<cmd>Telescope oldfiles<cr>",   desc = "Recent" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Buffers" },
+    { "<leader>f/", "<cmd>Telescope live_grep<cr>",  desc = "Grep" },
+    {
+      "<leader>fc",
+      function() require("telescope.builtin").colorscheme({ enable_preview = true }) end,
+      desc =
+      "Colorscheme",
+    },
+  },
+  config = function()
+    local actions = require("telescope.actions")
+
+    require("telescope").setup({
+      defaults = {
+        layout_config = {
+          preview_width = 0.55,
+        },
+        mappings = {
+          i = {
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-n>"] = actions.cycle_history_next,
+            ["<C-p>"] = actions.cycle_history_prev,
+          },
+        },
+      },
+    })
+  end,
 }
