bce8d44073067f02565c8f959919f21006f1952b
MikkelvtK <mvtklooster@hotmail.com>
2023-06-12 09:38:14 +0200

    update: telescope, integrate which-key

--- .config/nvim-dev/lua/plugins/telescope.lua
+++ .config/nvim-dev/lua/plugins/telescope.lua
@@ -1,38 +1,69 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.1",
-  cmd = "Telescope",
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  keys = {
-    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-    { "<leader>fr", "<cmd>Telescope oldfiles<cr>",   desc = "Recent" },
-    { "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Buffers" },
-    { "<leader>f/", "<cmd>Telescope live_grep<cr>",  desc = "Grep" },
-    { "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "Help Tags" },
-    {
-      "<leader>fc",
-      function() require("telescope.builtin").colorscheme({ enable_preview = true }) end,
-      desc =
-      "Colorscheme",
+  {
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make", }
     },
-  },
-  config = function()
-    local actions = require("telescope.actions")
-
-    require("telescope").setup({
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+      { "<leader>fr", "<cmd>Telescope oldfiles<cr>",   desc = "Recent" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Buffers" },
+      { "<leader>f/", "<cmd>Telescope live_grep<cr>",  desc = "Grep" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "Help Tags" },
+      {
+        "<leader>fc",
+        function() require("telescope.builtin").colorscheme({ enable_preview = true }) end,
+        desc =
+        "Colorscheme",
+      },
+    },
+    opts = {
       defaults = {
         layout_config = {
           preview_width = 0.55,
         },
         mappings = {
           i = {
-            ["<C-j>"] = actions.move_selection_next,
-            ["<C-k>"] = actions.move_selection_previous,
-            ["<C-n>"] = actions.cycle_history_next,
-            ["<C-p>"] = actions.cycle_history_prev,
+            ["<C-j>"] = function(...)
+              require("telescope.actions").move_selection_next(...)
+            end,
+            ["<C-k>"] = function(...)
+              require("telescope.actions").move_selection_previous(...)
+            end,
+            ["<C-n>"] = function(...)
+              require("telescope.actions").cycle_history_next(...)
+            end,
+            ["<C-p>"] = function(...)
+              require("telescope.actions").cycle_history_prev(...)
+            end,
           },
         },
       },
-    })
-  end,
+    },
+    config = function(_, opts)
+      local telescope = require("telescope")
+      telescope.setup(opts)
+      telescope.load_extension("fzf")
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      plugins = { spelling = true },
+      defaults = {
+        mode = { "n", "v", },
+        ["<leader>f"] = { name = "+File" },
+        ["<leader>q"] = { name = "+Quit" },
+        ["<leader>qq"] = { cmd = "<cmd>q<cr>", desc = "Quit" },
+      },
+    },
+    config = function(_, opts)
+      local wk = require("which-key")
+      wk.setup(opts)
+      wk.register(opts.defaults)
+    end
+  }
 }
