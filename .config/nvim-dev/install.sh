5dde6c3cf4da3c071cea8ea6b8d6e18fcb0dd640
MikkelvtK <mvtklooster@hotmail.com>
2023-06-20 20:40:39 +0200

    chore: add null-ls

--- .config/nvim-dev/install.sh
+++ .config/nvim-dev/install.sh
@@ -3,6 +3,7 @@
 # TODO: Check if dependencies are installed
 # - neovim
 # - ripgrep
+# - shellcheck
 
 NVD_APP_NAME=nvim-dev
 NVD_APP_CONFIG=~/.config/$NVD_APP_NAME
--- .config/nvim-dev/lua/setup/go.lua
+++ .config/nvim-dev/lua/setup/go.lua
@@ -26,7 +26,6 @@ return {
     config = function(_, opts)
       require("go").setup(opts)
     end,
-    event = { "CmdlineEnter" },
     ft = { "go", "gomod" },
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
