25485b417bbd47ecd5a861f40e2c860753dbcdf5
MikkelvtK <mvtklooster@hotmail.com>
2023-09-09 11:16:13 +0200

    chore: remove null-ls

--- .config/nvim-dev/lua/plugins/lsp/format.lua
+++ .config/nvim-dev/lua/plugins/lsp/format.lua
@@ -2,18 +2,10 @@ local M = {}
 
 function M.format()
   local buf = vim.api.nvim_get_current_buf()
-  local ft = vim.bo[buf].filetype
-  local have_nls = package.loaded["null-ls"] and
-      (#require("null-ls.sources").get_available(ft, "NULL_LS_FORMATTING") > 0)
 
   vim.lsp.buf.format {
     bufnr = buf,
-    filter = function(client)
-      if have_nls then
-        return client.name == "null-ls"
-      end
-      return client.name ~= "null-ls"
-    end,
+    filter = true
   }
 end
 
--- .config/nvim-dev/lua/plugins/lsp/init.lua
+++ .config/nvim-dev/lua/plugins/lsp/init.lua
@@ -22,20 +22,4 @@ return {
       },
     },
   },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    event = "BufReadPre",
-    dependencies = { "mason.nvim" },
-    opts = function()
-      local nls = require "null-ls"
-      return {
-        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-        sources = {
-          nls.builtins.formatting.shfmt,
-          nls.builtins.diagnostics.actionlint,
-          nls.builtins.formatting.yamlfix,
-        },
-      }
-    end,
-  },
 }
