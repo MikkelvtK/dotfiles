d17fc4893c0890aefa5f5fedae1c9a1d5e397bdd
MikkelvtK <mvtklooster@hotmail.com>
2023-09-12 10:38:29 +0200

    chore: add python support

--- .config/nvim-dev/lua/plugins/lsp/format.lua
+++ .config/nvim-dev/lua/plugins/lsp/format.lua
@@ -2,10 +2,18 @@ local M = {}
 
 function M.format()
   local buf = vim.api.nvim_get_current_buf()
+  local ft = vim.bo[buf].filetype
+  local have_nls = package.loaded["null-ls"] and
+      (#require("null-ls.sources").get_available(ft, "NULL_LS_FORMATTING") > 0)
 
   vim.lsp.buf.format {
     bufnr = buf,
-    filter = true
+    filter = function(client)
+      if have_nls then
+        return client.name == "null-ls"
+      end
+      return client.name ~= "null-ls"
+    end,
   }
 end
 
--- .config/nvim-dev/lua/plugins/lsp/init.lua
+++ .config/nvim-dev/lua/plugins/lsp/init.lua
@@ -22,4 +22,20 @@ return {
       },
     },
   },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "BufReadPre",
+    dependencies = { "mason.nvim" },
+    opts = function()
+      local nls = require "null-ls"
+      return {
+        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+        sources = {
+          nls.builtins.formatting.shfmt,
+          nls.builtins.diagnostics.actionlint,
+          nls.builtins.formatting.yamlfix,
+        },
+      }
+    end,
+  },
 }
