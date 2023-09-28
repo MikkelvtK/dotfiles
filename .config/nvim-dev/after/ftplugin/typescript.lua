fa4eb9d4f619711157bd22fd9d997cf002004738
MikkelvtK <mvtklooster@hotmail.com>
2023-09-28 19:45:26 +0200

    add: typescript support

---
+++ .config/nvim-dev/after/ftplugin/typescript.lua
@@ -0,0 +1,7 @@
+local bo = vim.bo
+
+-- General settings for typescript files
+bo.shiftwidth = 2
+bo.expandtab = true
+bo.tabstop = 2
+bo.softtabstop = 2
---
+++ .config/nvim-dev/lua/setup/typescript.lua
@@ -0,0 +1,98 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "javascript", "typescript", "tsx" })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "jose-elias-alvarez/typescript.nvim" },
+    opts = {
+      servers = {
+        ---Typescript
+        tsserver = {
+          settings = {
+            typescript = {
+              format = {
+                indentSize = vim.o.shiftwidth,
+                convertTabsToSpaces = vim.o.expandtab,
+                tabSize = vim.o.tabstop,
+              },
+            },
+            javascript = {
+              format = {
+                indentSize = vim.o.shiftwidth,
+                convertTabsToSpaces = vim.o.expandtab,
+                tabSize = vim.o.tabstop,
+              },
+            },
+            completions = {
+              completeFunctionCalls = true,
+            },
+          },
+        },
+        -- ESLint
+        eslint = {
+          settings = {
+            workingDirectory = { mode = "auto" },
+          },
+        },
+      },
+      setup = {
+        tsserver = function(_, opts)
+          require("plugins.lsp.utils").on_attach(function(client, bufnr)
+            if client.name == "tsserver" then
+              -- stylua: ignore
+              vim.keymap.set("n", "<leader>lo", "<cmd>TypescriptOrganizeImports<CR>",
+                { buffer = bufnr, desc = "Organize Imports" })
+              -- stylua: ignore
+              vim.keymap.set("n", "<leader>lR", "<cmd>TypescriptRenameFile<CR>", { desc = "Rename File", buffer = bufnr })
+            end
+          end)
+          require("typescript").setup { server = opts }
+          return true
+        end,
+        eslint = function()
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            callback = function(event)
+              local client = vim.lsp.get_active_clients({ bufnr = event.buf, name = "eslint" })[1]
+              if client then
+                local diag = vim.diagnostic.get(event.buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
+                if #diag > 0 then
+                  vim.cmd "EslintFixAll"
+                end
+              end
+            end,
+          })
+        end,
+      },
+    },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      table.insert(opts.sources, require "typescript.extensions.null-ls.code-actions")
+    end,
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/neotest-jest",
+      "marilari88/neotest-vitest",
+      "thenbe/neotest-playwright",
+    },
+    opts = function(_, opts)
+      vim.list_extend(opts.adapters, {
+        require "neotest-jest",
+        require "neotest-vitest",
+        require("neotest-playwright").adapter {
+          options = {
+            persist_project_selection = true,
+            enable_dynamic_test_discovery = true,
+          },
+        },
+      })
+    end,
+  },
+}
